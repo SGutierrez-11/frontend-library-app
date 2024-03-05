90527e4b4c9d064de5d6dfc2b0c55519f7cb16f8
Santiago Gutierrez <santicoguvi02@gmail.com>
2024-03-05 03:42:10 -0500

    Doing the frontend to add, update and delete books, and show the books

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { Container, Grid, Button, Modal, Box, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import BookCard from './components/BookCard';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [books, setBooks] = useState([]);
+    const [showAddBookModal, setShowAddBookModal] = useState(false);
+    const [newBook, setNewBook] = useState({});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await axios.get('http://localhost:5087/api/Book');
+                setBooks(response.data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
 
-export default App
+        fetchBooks();
+    }, []);
+
+    const handleAddBook = async () => {
+        try {
+            // Construir el objeto de libro con el formato correcto
+            const formattedNewBook = {
+                id: 0,
+                title: newBook.title,
+                author: newBook.author,
+                created: newBook.created,
+                image: newBook.image
+            };
+
+            await axios.post('http://localhost:5087/api/Book', formattedNewBook);
+
+            // Recargar la lista de libros después de agregar un nuevo libro
+            const response = await axios.get('http://localhost:5087/api/Book');
+            setBooks(response.data);
+
+            // Ocultar el modal después de agregar el libro
+            setShowAddBookModal(false);
+
+            // Limpiar los campos del nuevo libro
+            setNewBook({});
+        } catch (error) {
+            console.error('Error adding book:', error);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setNewBook({ ...newBook, [name]: value });
+    };
+
+    return (
+        <Container>
+            <Typography variant="h3" align="center" gutterBottom>
+                Library APP
+            </Typography>
+            <Typography variant="h5" align="center" gutterBottom>
+                ¡Agrega libros para expandir tu biblioteca!
+            </Typography>
+            <Button onClick={() => setShowAddBookModal(true)} variant="contained" color="success" size="large" sx={{ display: 'block', margin: 'auto' }}>
+                Agregar libro
+            </Button>
+            <Modal open={showAddBookModal} onClose={() => setShowAddBookModal(false)}>
+                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', bgcolor: 'background.paper', p: 4, width: 400, maxWidth: '90%' }}>
+                    <Typography variant="h6" gutterBottom>Agregar libro</Typography>
+                    <TextField name="title" label="Título" fullWidth onChange={handleChange} />
+                    <TextField name="author" label="Autor" fullWidth onChange={handleChange} />
+                    <TextField name="created" label="Fecha de creación" fullWidth onChange={handleChange} />
+                    <TextField name="image" label="URL de la imagen" fullWidth onChange={handleChange} />
+                    <Button onClick={handleAddBook} variant="contained" sx={{ mt: 2 }}>Agregar</Button>
+                </Box>
+            </Modal>
+            <Grid container spacing={3} sx={{ marginTop: 4 }}>
+                {books.map((book) => (
+                    <Grid item xs={12} sm={6} md={4} key={book.id}>
+                        <BookCard book={book} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
+};
+
+export default App;
---
+++ src/components/BookCard.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { IconButton, Card, CardContent, Typography, CardActions, Modal, Box, TextField, Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import axios from 'axios';
+
+const BookCard = ({ book, onUpdate, onDelete }) => {
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const [updatedBook, setUpdatedBook] = useState(book);
+
+    const handleUpdateBook = async () => {
+        try {
+            await axios.put(`http://localhost:5087/api/Book/${updatedBook.id}`, updatedBook);
+            // Cerrar el modal después de la actualización y actualizar los libros
+            setShowUpdateModal(false);
+            onUpdate(); // Actualizar la lista de libros
+        } catch (error) {
+            console.error('Error updating book:', error);
+        }
+    };
+
+    const handleDeleteBook = async () => {
+        try {
+            await axios.delete(`http://localhost:5087/api/Book/${book.id}`);
+            // Llamar a la función de eliminación pasada como prop
+            onDelete(book.id);
+        } catch (error) {
+            console.error('Error deleting book:', error);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setUpdatedBook({ ...updatedBook, [name]: value });
+    };
+
+    return (
+        <Card>
+            <img src={book.image} alt={book.title} style={{ width: '100%' }} />
+            <CardContent>
+                <Typography variant="h5" component="h2" sx={{ fontWeight: 'bold', fontSize: 18 }}>
+                    {book.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <strong>Author:</strong> {book.author}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <strong>Published:</strong> {book.created}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <IconButton aria-label="update" onClick={() => setShowUpdateModal(true)} sx={{ color: 'orange' }}>
+                    <ChangeCircleIcon />
+                </IconButton>
+                <IconButton aria-label="delete" onClick={handleDeleteBook} sx={{ color: 'red' }}>
+                    <DeleteIcon />
+                </IconButton>
+            </CardActions>
+
+            {/* Modal para actualizar información del libro */}
+            <Modal open={showUpdateModal} onClose={() => setShowUpdateModal(false)}>
+                <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', bgcolor: 'background.paper', p: 4, width: 400, maxWidth: '90%' }}>
+                    <Typography variant="h6" gutterBottom>Actualizar libro</Typography>
+                    <TextField name="title" label="Título" fullWidth value={updatedBook.title} onChange={handleChange} />
+                    <TextField name="author" label="Autor" fullWidth value={updatedBook.author} onChange={handleChange} />
+                    <TextField name="created" label="Fecha de creación" fullWidth value={updatedBook.created} onChange={handleChange} />
+                    <TextField name="image" label="URL de la imagen" fullWidth value={updatedBook.image} onChange={handleChange} />
+                    <Button onClick={handleUpdateBook} variant="contained" sx={{ mt: 2 }}>Guardar</Button>
+                </Box>
+            </Modal>
+        </Card>
+    );
+};
+
+export default BookCard;
